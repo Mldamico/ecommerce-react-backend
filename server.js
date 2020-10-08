4b7b75331ba4a49ce46a090669ae9750a584205b
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-10-08 18:55:49 -0300

    Error handling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import productRoutes from './routes/productsRoute.js';
 import colors from 'colors';
 import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 dotenv.config();
 connectDB();
 const app = express();
@@ -12,6 +13,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 4444, () => {
   console.log(
