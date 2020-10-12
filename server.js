00c3c837309f4fae6a1dcb71d971385e0f04d7c6
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-10-12 19:41:24 -0300

    Order controller and order routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import productRoutes from './routes/productsRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
@@ -17,6 +18,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 app.use(notFound);
 
 app.use(errorHandler);
