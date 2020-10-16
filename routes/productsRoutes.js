a53eb410dbca6d1d7d817adab1397828a348ecaf
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-10-16 18:24:30 -0300

    Carousel

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -6,13 +6,16 @@ import {
   deleteProduct,
   getProductById,
   getProducts,
+  getTopProducts,
   updateProduct,
 } from '../controllers/productController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+
 router.route('/:id/reviews').post(protect, createNewReview);
+router.route('/top').get(getTopProducts);
 router
   .route('/:id')
   .get(getProductById)
